9e246c6c01574806df38b1654e6d9a4a0de64e16
pedram <peeeedii@gmail.com>
2024-01-19 12:16:10 +0330

    refactor: get breeds with react query

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,32 +1,8 @@
-import { useEffect, useState } from "react";
-
-const localCache = {};
+import { useQuery } from "react-query";
+import fetchBreedList from "./fetchBreedList";
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setStatus("loading");
-      setBreedList([]);
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`,
-      );
-      const json = await res.json();
-      setStatus("loaded");
-      setBreedList(json.breeds);
-      localCache[animal] = json.breeds;
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  return [results?.data?.breeds ?? [], results.status];
 }
