864511306f5207246b6eaa77c44d6e65d4d71dd2
pedram <peeeedii@gmail.com>
2024-01-21 22:30:29 +0330

    feat: add loader component

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,17 +2,14 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import Loader from "./Loader";
 
 const Details = () => {
   const { id } = useParams();
   const { isLoading, data } = useQuery(["details", id], fetchPet);
 
   if (isLoading) {
-    return (
-      <div className="loading-pane">
-        <h2 className="loader">🌀</h2>
-      </div>
-    );
+    return <Loader />;
   }
 
   const pet = data.pets[0];
---
+++ src/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="loading-pane">
+      <h2 className="loader">🌀</h2>
+    </div>
+  );
+}
