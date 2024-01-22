c5be7e0d80fb79cca93d66fe89e2209050d1e770
pedram <peeeedii@gmail.com>
2024-01-22 21:17:50 +0330

    chore: change the modal to ts
    i have chosen modal cause it has no dependency to other files

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useRef } from "react";
+import { ReactElement, ReactNode, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
-  const elRef = useRef(null);
+const Modal = ({ children }: { children: ReactElement }) => {
+  const elRef = useRef<HTMLDivElement | null>(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      if (elRef.current) modalRoot.removeChild(elRef.current);
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
