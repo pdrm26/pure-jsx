4d2ea2236a1f01f9a7b3ee041cfda24e48d2684c
pedram <peeeedii@gmail.com>
2024-01-25 22:59:06 +0330

    feat: add vitest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  test: {
+    environment: "happy-dom" //by default it's gonna use js-dom which is heavier and slower than happy-dom
+  }
 });
