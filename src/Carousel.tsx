16559e224f1626d38cc2371865796007647b7686
pedram <peeeedii@gmail.com>
2024-01-25 23:58:27 +0330

    feat(test): add carousel test

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -25,11 +25,12 @@ export default class Carousel extends Component<Props> {
     const { images } = this.props;
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal" />
+        <img data-testid="hero" src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
             // eslint-disable-next-line
             <img
+              data-testid={`thumbnail${index}`}
               key={photo}
               src={photo}
               className={index === active ? "active" : ""}
---
+++ src/__tests__/Carousel.test.tsx
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+import Carousel from "../Carousel";
+import { render } from "@testing-library/react";
+
+test("lets users click on thumbnails to make them the hero", async () => {
+  const images = ["1.jpg", "2.jpg", "3.jpg"];
+  const carousel = render(<Carousel images={images} />);
+  const hero = (await carousel.findByTestId("hero")) as HTMLImageElement;
+  expect(hero.src).toContain(images[0]);
+
+  for (const [index, image] of images.entries()) {
+    const thumb = await carousel.findByTestId(`thumbnail${index}`);
+    await thumb.click();
+    expect(hero.src).toContain(image);
+    expect(Array.from(thumb.classList)).toContain("active");
+  }
+
+  carousel.unmount();
+});
